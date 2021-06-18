52596c7f5e74964911f063d96fe8806874681be7
Paula Coll <paula.coll@bbva.com>
2021-06-18 16:08:39 +0200

    Add function to retrieve book details: price, tags and bookstores number.

--- todostuslibros/todostuslibros/spiders/todostuslibros_spider.py
+++ todostuslibros/todostuslibros/spiders/todostuslibros_spider.py
@@ -1,19 +1,31 @@
 import scrapy
 import todostuslibros.settings as settings
-
+import re
 class TodostuslibrosSpider(scrapy.Spider):
     name = "todostuslibros"
 
     def start_requests(self):
         for url in settings.TODOSTUSLIBROS_URL_LIST:
-            yield scrapy.Request(url=url, callback=self.parse)
+            yield scrapy.Request(url=url, callback=self.parse_book_list)
 
-    def parse(self, response):
+    def parse_book_list(self, response):
         for book in response.css("div.book-details"):
-            yield {
-                'title' : book.css("h2.title a::text").get().strip(),
-                'author' : book.css("h3.author a::text").get(),
-                'data' : book.css("p.data::text").get()
-            }
+            meta = {}
+            meta['title'] = book.css("h2.title a::text").get().strip(),
+            meta['author'] = book.css("h3.author a::text").get(),
+            data = book.css("p.data::text").get().split('/')
+            meta['publisher'] = data[0].strip()
+            meta['isbn'] = re.sub(r'\D', '', data[1].strip())
 
-        yield from response.follow_all(css='nav a.page-link[rel="next"]::attr("href")', callback=self.parse)
+            book_detail_url = settings.TODOSTUSLIBROS_BOOK_DETAIL_URL_TEMPLATE.replace('{isbn}', meta['isbn'])
+
+            yield scrapy.Request(url=book_detail_url, callback=self.parse_book_detail, cb_kwargs={'meta':meta})
+        
+        yield from response.follow_all(css='nav a.page-link[rel="next"]::attr("href")', callback=self.parse_book_list)
+
+    def parse_book_detail(self, response, meta):
+        meta['price'] = response.css('.book-price strong::text').get()
+        meta['tags'] = response.css('.row.materias a::text').get()
+        meta['bookstores_number'] = int(response.css('.before-title::text').re(r'\d+')[0])
+
+        yield meta
